3aa90e7643ba062cb0f953091486e1a308bd566f
bronzedeco9. <victoronose03@gmail.com>
2024-11-08 10:38:25 +0200

    Removed console.log commands.

--- script.js
+++ script.js
@@ -3,27 +3,21 @@
 let secretNumber = Math.trunc(Math.random() * 1000 + 1);
 console.log(secretNumber);
 let score = 20;
-
 let highscore = 0;
-
 document.querySelector('.number').textContent = '?';
-
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
-
     if (!guess) {
         document.querySelector('.message').textContent = 'No number!';
     } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Correct number! Te iubesc <3'
-
+        document.querySelector('.message').textContent = 'Correct number!'
         document.querySelector('body').style.backgroundColor = '#60b347';
-
         document.querySelector('.number').style.width = '30rem';
         document.querySelector('.number').textContent = secretNumber;
-        highscore = score;
-        document.querySelector('.highscore').textContent = highscore;
-
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
     } else if (guess < secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = 'Too low! :('
@@ -51,7 +45,7 @@ document.querySelector('.again').addEventListener('click', function () {
     score = 20;
     document.querySelector('.score').textContent = score;
     document.querySelector('.message').textContent = 'Start guessing...';
-    console.log(secretNumber)
+    console.log(secretNumber);
     document.querySelector('.number').textContent = "?";
     document.querySelector('.number').style.width = '15rem';
     document.querySelector('.guess').value = '';
